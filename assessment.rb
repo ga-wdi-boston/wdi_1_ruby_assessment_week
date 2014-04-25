4800b6d0857dcdbcf91d05ea572d28230a689a31
Kathleen Votolato <kvotolato3@gmail.com>
2014-04-25 10:05:16 -0400

    Completed assessment

--- assessment.rb
+++ assessment.rb
@@ -1,14 +1,16 @@
-# Just breathe. I just want to see how you're doing. 
+# Just breathe. I just want to see how you're doing.
 
 
 #### Question 1
-# Consider the following. What is the value of the variable `cats`? 
+# Consider the following. What is the value of the variable `cats`?
 # Do *not* check these values in Ruby/pry
 
 number_of_dogs = 15
 cats = number_of_dogs - 2
 forty_two = "answer to everything"
 cats = forty_two
+
+# "answer to everything"
 
 
 
@@ -23,13 +25,17 @@ g = e + f
 f == g
 e = "bar"
 
-
+# e = "bar", g = "foobar", f = "bar"
 
 #### Question 3
-# In a comment, write the difference between these two statements. 
+# In a comment, write the difference between these two statements.
 
 weather = "cloudy"
-weather != "sunny" 
+weather != "sunny"
+
+# the first one is an assignment of the string "cloudy" to the variable weather.
+# The second one is an evaluation of whether the variable weather is not equal (!= means not equal)
+# to the string "sunny".
 
 
 
@@ -43,32 +49,47 @@ x = 2
 end
 x = x - 1
 
+# 4
+
 
 
 #### Question 5
-# What is the value of the variable 'ok' after these lines? 
+# What is the value of the variable 'ok' after these lines?
 # Do not check these in Ruby/pry
 
 ingredients = ["tomatos", "wheat", "soy", "dairy"]
 ok = ingredients.include?("walnuts") ? false : true
+
+# false
 
 #### Question 6
 # On a new line, write code to insert the value `snake` at the end of the on_airplane array. Don't modify the original array creation line.
 
 on_airplane = ['cat', 'dog', 'people']
 
+on_airplane.push('snake')
+
 #### Question 7
 # Write code to return the value of the key :age in the friend hash
 chris = {name: "Chris", favorite_food: "Pizza", age: 27, friends: ["Sam", "Kelly"] }
 
+chris[:age]
+
 #### Question 8
 # Add a key of :hair_color with a value of "blue" to the `chris` array
 
+# Is this a trick question? key value pairs don't exist in arrays.
+# I'm going to assume you are still talking about the chris hash above.
+chris[:hair_color] = 'blue'
+
 #### Question 9
 # Write code to retrieve the index of 'dog' in the on_airplane array.
+on_airplane.index('dog')
 
 #### Question 10
 # Write code to add "Kim" to the array of friends inside the 'chris' hash.
+
+chris[:friends] += ["Kim"]
 
 #### Question 11
 # Consider the following method
@@ -80,6 +101,9 @@ end
 y = square(2)
 # What is the value of 'x' at this line? Do not check in Ruby/pry
 
+# x is an undefined local variable at this point because
+# we are outside the scope of the method
+
 #### Question 12
 # If you were to invoke this method, what would print to the screen?
 # Do not actually invoke the method.
@@ -87,6 +111,9 @@ y = square(2)
 def my_name()
   puts name
 end
+
+# We would get an error saying name is an undefined local variable
+# or method.
 
 #### Question 13
 
@@ -99,6 +126,8 @@ end
 
 age = 10
 get_older(age)
+
+# age = 10 because we never reassign the output of the get_older(age) to anything.
 
 
 #### Question 14
@@ -125,25 +154,49 @@ end
 energy = 2
 jump()
 
+# An error. how_high doesn't have any local variables defined for it, so it won't work. And
+# since jump uses how_high, that won't work either.
 
 #### Question 16
 # Create a class called 'Pet'. Also create a class called 'Dog' that inherits from 'Pet'
+
+class Pet
+end
+class Dog < Pet
+end
 
 
 #### Question 17
 # Create a new instance of a Dog, and store it as my_dog
 
+my_dog = Dog.new
+
 
 #### Question 18
 # Add the instance method 'bark' to Dog, which will print "bark park" to the screen.
+
+class Dog < Pet
+  def bark
+    puts 'bark bark'
+  end
+end
 
 
 #### Question 19
 # Create a class called 'Pug' that inherits from 'Dog'
 # Make the Pug class have its own bark method that will print "wuff wuff" to the screen.
 
+class Pug < Dog
+  def bark
+    puts 'wuf wuf'
+  end
+end
+
 
 #### Question 20
 # Create a new Pug instance, store it as my_pug, and make it bark
 
+my_pug = Pug.new
+
+my_pug.bark
 
